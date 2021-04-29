20d2ada3cac4881dedcd1e542b8299149984b336
Tiewly <tananporn2112@gmail.com>
2021-04-30 02:01:47 +0700

    Add sidebar in web

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,12 @@
 import "../styles/globals.css";
 import "semantic-ui-css/semantic.min.css";
+import SideBar from "../component/SideBar";
+import "../styles/sideBar.css";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <div>
-      <h1>Luv U</h1>
+    <div style={{ display: "flex" }}>
+      <SideBar />
       <Component {...pageProps} />
     </div>
   );

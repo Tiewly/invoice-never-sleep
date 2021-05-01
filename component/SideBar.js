2dfced0211a5323f4f133a08f189f008c484dca2
Tiewly <tananporn2112@gmail.com>
2021-05-01 20:14:44 +0700

    Add route & Edit sidebar

--- component/SideBar.js
+++ component/SideBar.js
@@ -1,23 +1,30 @@
 import React from "react";
 import { Icon, Button } from "semantic-ui-react";
 
+var type = "";
 export default function sideBar() {
   return (
     <div className="sidebar">
       <h2>NVSL - IMS</h2>
       <div className="menu">
-        <div className="menu-item">
-          <Icon name="home" />
-          <p> Dashboard</p>
-        </div>
-        <div className="menu-item">
-          <Icon name="group" />
-          <p> Partner</p>
-        </div>
-        <div className="menu-item">
-          <Icon name="dollar sign" />
-          <p> Invoice</p>
-        </div>
+        <a href="#">
+          <div className="menu-item">
+            <Icon name="home" />
+            <p> Dashboard</p>
+          </div>
+        </a>
+        <a href="#">
+          <div className="menu-item">
+            <Icon name="group" />
+            <p> Partner</p>
+          </div>
+        </a>
+        <a href="/">
+          <div className="menu-item-open">
+            <Icon name="dollar sign" />
+            <p> Invoice</p>
+          </div>
+        </a>
       </div>
     </div>
   );
